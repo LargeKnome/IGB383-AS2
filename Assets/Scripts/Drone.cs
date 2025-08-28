e8528fe6f61e3c0d1cda59d678cef77efb406b41
LargeKnome <44670186+LargeKnome@users.noreply.github.com>
2025-08-28 11:48:03 +1000

    Added project base

---
+++ Assets/Scripts/Drone.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Drone : Enemy {
+
+    GameManager gameManager;
+
+    Rigidbody rb;
+
+    //Movement & Rotation Variables
+    public float speed = 50.0f;
+    private float rotationSpeed = 5.0f;
+    private float adjRotSpeed;
+    private Quaternion targetRotation;
+    public GameObject target;
+    public float targetRadius = 200f;
+
+    //Boid Steering/Flocking Variables
+
+    // Use this for initialization
+    void Start() {
+
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        //Acquire player if spawned in
+        if (gameManager.gameStarted)
+            target = gameManager.playerDreadnaught;
+
+        //Move towards valid targets
+        if(target)
+            MoveTowardsTarget();
+
+    }
+
+    private void MoveTowardsTarget() {
+        //Rotate and move towards target if out of range
+        if (Vector3.Distance(target.transform.position, transform.position) > targetRadius) {
+
+            //Lerp Towards target
+            targetRotation = Quaternion.LookRotation(target.transform.position - transform.position);
+            adjRotSpeed = Mathf.Min(rotationSpeed * Time.deltaTime, 1);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, adjRotSpeed);
+
+            rb.AddRelativeForce(Vector3.forward * speed * 20 * Time.deltaTime);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Mothership.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mothership : MonoBehaviour {
+
+    public GameObject enemy;
+    public int numberOfEnemies = 20;
+
+    public GameObject spawnLocation;
+
+    // initialise the boids
+    void Start() {
+
+        for (int i = 0; i < numberOfEnemies; i++) {
+
+            Vector3 spawnPosition = spawnLocation.transform.position;
+
+            spawnPosition.x = spawnPosition.x + Random.Range(-50, 50);
+            spawnPosition.y = spawnPosition.y + Random.Range(-50, 50);
+            spawnPosition.z = spawnPosition.z + Random.Range(-50, 50);
+
+            Instantiate(enemy, spawnPosition, spawnLocation.transform.rotation);
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
+
