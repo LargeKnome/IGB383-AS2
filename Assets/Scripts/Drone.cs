78d2f946059980a3b1c2d6798d286a00a42afe51
LargeKnome <44670186+LargeKnome@users.noreply.github.com>
2025-09-17 16:30:34 +1000

    Workshop 1 Implementation

--- Assets/Scripts/Drone.cs
+++ Assets/Scripts/Drone.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Unity.Mathematics;
 
 public class Drone : Enemy {
 
@@ -16,6 +17,12 @@ public class Drone : Enemy {
     public float targetRadius = 200f;
 
     //Boid Steering/Flocking Variables
+    [SerializeField] float seperationDistance;
+    [SerializeField] float cohesionDistance;
+    [SerializeField] float seperationStrength;
+    [SerializeField] float cohesionStrength;
+    Vector3 cohesionPos = Vector3.zero;
+    int boidIndex = 0;
 
     // Use this for initialization
     void Start() {
@@ -30,12 +37,13 @@ public class Drone : Enemy {
 
         //Acquire player if spawned in
         if (gameManager.gameStarted)
-            target = gameManager.playerDreadnaught;
+            target = gameManager.playerDreadnaught.transform.GetChild(0).gameObject;
 
         //Move towards valid targets
         if(target)
             MoveTowardsTarget();
 
+        BoidBehaviour();
     }
 
     private void MoveTowardsTarget() {
@@ -49,6 +57,38 @@ public class Drone : Enemy {
 
             rb.AddRelativeForce(Vector3.forward * speed * 20 * Time.deltaTime);
         }
+    }
+
+    private void BoidBehaviour()
+    {
+        boidIndex++;
+        if (boidIndex >= gameManager.enemyList.Length)
+        {
+            Vector3 cohesiveForce = (cohesionStrength / Vector3.Distance(cohesionPos, transform.position)) * (cohesionPos - transform.position);
+
+            rb.AddForce(cohesiveForce); 
+            boidIndex = 0;
+            cohesionPos = Vector3.zero;
+        }
+
+        Vector3 pos = gameManager.enemyList[boidIndex].transform.position;
+        Quaternion rot = gameManager.enemyList[boidIndex].transform.rotation;
+        float dist = Vector3.Distance(transform.position, pos);
+
+        if (dist > 0f) 
+        {
+            if (dist <= seperationDistance)
+            {
+                float scale = seperationStrength / dist;
+                rb.AddForce(scale * Vector3.Normalize(transform.position - pos));
+            }
+            else if (dist < cohesionDistance && dist > seperationDistance)
+            {
+                cohesionPos = cohesionPos + pos * (1f / (float)gameManager.enemyList.Length);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, 1f);
+            }
+        }
+
     }
 
 }
